7d79b6a8e059e9f101870955276eb661576fc052
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-02 14:39:19 +0000

    feat(note-entry): new entity to note entries

---
+++ src/domain/notes/enterprise/entities/note-entry.ts
@@ -0,0 +1,29 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+import { EntryProps } from "./entry";
+
+export interface NoteEntryProps extends EntryProps {
+  entryId: UniqueEntityId
+}
+
+export class NoteEntry extends Entity<NoteEntryProps> {
+  get entryId(): UniqueEntityId {
+    return this.props.entryId
+  }
+
+  static create(
+    props: Optional<NoteEntryProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const noteEntry = new NoteEntry(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    )
+
+    return noteEntry
+  }
+}
