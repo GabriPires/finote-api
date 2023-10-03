ed1df9a4cafeb45cc102c1d80fce83cd65cc1545
Gabriel Pires <gabpiresdev@gmail.com>
2023-10-03 12:32:03 +0000

    feat(entry): remove entry use case and tests done

--- src/domain/notes/application/repositories/note-entries-repository.ts
+++ src/domain/notes/application/repositories/note-entries-repository.ts
@@ -3,4 +3,5 @@ import { NoteEntry } from '../../enterprise/entities/note-entry'
 export interface NoteEntriesRepository {
   findManyByNoteId(noteId: string): Promise<NoteEntry[]>
   deleteManyByNoteId(noteId: string): Promise<void>
+  create(noteEntry: NoteEntry): Promise<void>
 }
--- test/repositories/in-memory-note-entries-repository.ts
+++ test/repositories/in-memory-note-entries-repository.ts
@@ -12,13 +12,15 @@ export class InMemoryNoteEntriesRepository implements NoteEntriesRepository {
     return Promise.resolve(entries)
   }
 
+  async create(noteEntry: NoteEntry): Promise<void> {
+    this.items.push(noteEntry)
+  }
+
   async deleteManyByNoteId(noteId: string): Promise<void> {
     const index = this.items.findIndex(
       (item) => item.noteId.toString() === noteId,
     )
 
     this.items.splice(index, 1)
-
-    return Promise.resolve()
   }
 }
